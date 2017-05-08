a1a470e10421daed81c375e55b0f393fe0ed38a2
zhoujun <zhoujun247@foxmail.com>
2017-05-08 19:00:25 +0800

    update

--- src/base/context.js
+++ src/base/context.js
@@ -2,12 +2,4 @@
  * Created by zhoujun on 2017/4/9.
  * email :zhoujun247@gmail.com
  */
-
-const context_proto = require('koa/lib/context');
-
-
-const proto = module.exports = {
-
-}
-
-const context  =  module.exports = Object.assign(context_proto,proto);
+const Koa = require('koa');
