6a7b36f049bd23274c68a34f8c75b97e7bc49d2f
zhoujun <zhoujun247@foxmail.com>
2017-06-09 15:11:27 +0800

    update Code

--- src/action.js
+++ src/action.js
@@ -16,6 +16,5 @@ module.exports = function ( action ) {
 			ctx.status = 500;
 		}
 
-
 	}
 }
--- src/router.js
+++ src/router.js
@@ -15,29 +15,28 @@ class Router extends KoaRouter{
 				let k = key.split(' ');
 				let method = k[0];
 				let path = k[1];
+				let routermiddlewareArr = []
 				if(['GET','POST','PUT','DELETE'].includes(method)){
 					if(method !='GET'){
-						this.use(bodyparse());		//GET方法没有body，就别加载这个中间件浪费资源了。
+						routermiddlewareArr.push(bodyparse());
 					}
-
 					try{
 						let controllerArr = router[key]['controller'].split('.');
-						compose(middlewares);
 						if(router[key]['middleware'] ){
 							for(let val of middlewares){
 								if(val.name == router[key]['middleware'] ){
-									this.use(val);
+									routermiddlewareArr.push(val);
 								}
 							}
 						}
 						for(let val of controllers){
 							if(val[controllerArr[1]]){
 								let action = val[controllerArr[1]];		//方法
-								this.register(path,[method],	_action(action));
+								routermiddlewareArr.push(_action(action));
+								this.register(path,[method],	compose(routermiddlewareArr));
 							}
 						}
 						console.log(`自动加载路由${path}成功`);
-
 					}catch (e){
 						console.error(e);
 						new Error('路由定义的控制器名称不正确',e);
